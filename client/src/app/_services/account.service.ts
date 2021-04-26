c9c598cb78c0e79015994a22428be1ebd898a595
Jason John <jasonjohn12@gmail.com>
2021-04-26 16:15:03 -0500

    Develop (#3)
    * Added  typescript and member details component
    
    * ability to edit a user
    
    * Add PhotoService using Cloudinary
    
    * working on photo upload and main profile picture

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,27 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
-import {map} from'rxjs/operators'
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(model: any) {
     console.log('baseURl', this.baseUrl)
     return this.http.post(this.baseUrl + 'account/login', model).pipe(
       map((response: User) => {
         const user = response;
-        if(user) {
-          localStorage.setItem('user', JSON.stringify(user));
-         this.currentUserSource.next(user)
+        if (user) {
+          this.setCurrentUser(user);
         }
       })
     );
@@ -29,18 +28,16 @@ export class AccountService {
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'account/register', model).pipe(
-      map((user: User)=> {
-        if(user) {
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUserSource.next(user);
+      map((user: User) => {
+        if (user) {
+          this.setCurrentUser(user);
         }
-       
       })
-    )
+    );
   }
-  setCurrentUser(user:User) {
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
-   
   }
 
   logout() {
